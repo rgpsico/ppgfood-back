43d6fe5767d02257912ad777542e8e0bf74c9c84
rgpsico <gerro121@hotmail.com>
2022-01-21 23:08:08 -0300

    database

--- database/migrations/2014_02_26_195553_create_tenants_table.php
+++ database/migrations/2014_02_26_195553_create_tenants_table.php
@@ -15,7 +15,7 @@ class CreateTenantsTable extends Migration
     {
         Schema::create('tenants', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('plan_id');
+            $table->unsignedBigInteger('plan_id')->nullable();
             $table->uuid('uuid');
             $table->string('cnpj')->unique();
             $table->string('name')->unique();
