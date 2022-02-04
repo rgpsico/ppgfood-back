2349e6e4ec722aeb44eb8d8c0e8d5645d02fcd39
rgpsico <gerro121@hotmail.com>
2022-02-04 10:26:37 -0300

    migrate add filed in client table

---
+++ database/migrations/2022_02_04_102504_add_filed_client_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFiledClientTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {         
+            $table->string('endereco');
+            $table->string('celular')->unique();
+            $table->string('instagran')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
