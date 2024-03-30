9dbfa2dc8552ab0bfcbe3b4875e0bf9aa146340b
roger <rogernevesn@gmail.com>
2024-03-30 12:12:59 -0300

    AJUSTES

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -30,4 +30,9 @@ class Client extends Authenticatable
     {
         return $this->hasMany(Evaluation::class);
     }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
