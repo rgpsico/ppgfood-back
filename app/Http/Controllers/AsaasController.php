56880581d135d507098ecf369cc47dc19402969e
roger <rogernevesn@gmail.com>
2024-11-07 01:07:27 -0300

    AJUSTES

--- app/Http/Controllers/AsaasController.php
+++ app/Http/Controllers/AsaasController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Services\AsaasService;
 use App\Services\ClientService;
+use App\Models\Client;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
