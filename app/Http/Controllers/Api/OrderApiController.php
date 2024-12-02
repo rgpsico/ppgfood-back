fdeee76e69b69b970e98ffaf87cc99da76c85892
roger <rogernevesn@gmail.com>
2024-12-02 16:06:53 -0300

    ajustes

--- app/Http/Controllers/Api/OrderApiController.php
+++ app/Http/Controllers/Api/OrderApiController.php
@@ -49,7 +49,7 @@ class OrderApiController extends Controller
 
     public function enviarPedidoEntregador($data)
     {
-        $url = 'http://www.comunidadeppg.com.br:3000/enviar-json';
+        $url = 'http://www.comunidadeppg.com.br:3000/enviarpedidoparaentregadores';
 
         $client = new Client();
 
