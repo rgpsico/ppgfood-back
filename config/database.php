27dc9cc4bd8f81631b6f9de779880a04cccb4a4c
roger <rogernevesn@gmail.com>
2025-03-13 12:35:57 -0300

    ajustes

--- config/database.php
+++ config/database.php
@@ -50,6 +50,20 @@ return [
 
     'connections' => [
 
+        'entregadores' => [
+            'driver' => 'mysql',
+            'host' => env('DB_ENTREGADORES_HOST', '127.0.0.1'),
+            'port' => env('DB_ENTREGADORES_PORT', '3306'),
+            'database' => env('DB_ENTREGADORES_DATABASE', 'entregadores'),
+            'username' => env('DB_ENTREGADORES_USERNAME', 'root'),
+            'password' => env('DB_ENTREGADORES_PASSWORD', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
