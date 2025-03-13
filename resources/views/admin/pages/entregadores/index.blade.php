d4d256cfeb739a718c5be711b43ef673c92fd7d8
roger <rogernevesn@gmail.com>
2025-03-13 15:58:21 -0300

    ajustes

--- resources/views/admin/pages/entregadores/index.blade.php
+++ resources/views/admin/pages/entregadores/index.blade.php
@@ -1,14 +1,32 @@
-<!DOCTYPE html>
-<html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin - Entregadores</title>
-    @vite(['resources/js/app.js'])
-</head>
-<body>
-    <div id="app">
+@extends('adminlte::page')
+
+@section('title', 'Pedidos')
+
+@section('content_header')
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ route('admin.index') }}">Dashboard</a></li>
+        <li class="breadcrumb-item active"><a href="{{ route('orders.index') }}" class="active">Pedidos</a></li>
+    </ol>
+
+    <h1>Pedidos</h1>
+@stop
+
+@section('content')
+    <div id="app" class="card">
+        
+
         <entregadores></entregadores>
     </div>
-</body>
-</html>
+@stop
+
+@section('adminlte_js')
+<script src="{{ asset('js/app.js') }}"></script>
+@stop
+
+@push('scripts-header')
+<script>
+    window.Laravel = {!! json_encode([
+        'tenantId' => auth()->check() ? auth()->user()->tenant_id : ''
+    ]) !!}
+</script>
+@endpush
