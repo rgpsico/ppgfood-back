a455a3c95d835551db4b922bbb5a78a01283003e
roger <rogernevesn@gmail.com>
2025-03-17 12:24:47 -0300

    ajustes

--- app/Http/Controllers/Api/UsuarioController.php
+++ app/Http/Controllers/Api/UsuarioController.php
@@ -22,7 +22,7 @@ class UsuarioController extends Controller
     {
         $validated = $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|email|unique:usuarios,email',
+            'email' => 'required|email|unique:entregadores.usuarios,email',
             'password' => 'required|string|min:6|confirmed',
         ]);
 
