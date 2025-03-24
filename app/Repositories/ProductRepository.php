1930c354d84ac356646f92e945f9a9c7bc6c76ca
roger <rogernevesn@gmail.com>
2025-03-24 17:03:19 -0300

    aa

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -21,9 +21,9 @@ class ProductRepository implements ProductRepositoryInterface
             ->join('categories', 'categories.id', '=', 'category_product.category_id')
             ->where('products.tenant_id', $idTenant)
             //->where('products.is_active', true) // Garantir que só produtos ativos sejam retornados
-            ->when(!empty($categories), function ($query) use ($categories) {
-                $query->whereIn('categories.uuid', $categories);
-            })
+            // ->when(!empty($categories), function ($query) use ($categories) {
+            //     $query->whereIn('categories.uuid', $categories);
+            // })
             ->select('products.*')
             ->distinct()
             ->get();
